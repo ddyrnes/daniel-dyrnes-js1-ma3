84c5c4c9747e79b9f0adb33c73e6f961ee0c14e0
ddyrnes <dev.dyrnes@gmail.com>
2022-08-25 13:01:02 +0200

    Update on question2-js

--- question-2.js
+++ question-2.js
@@ -1,9 +1,19 @@
 apiKey = "1adb635733d147ed887a956df85f5898";
 url = `https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=${apiKey}`;
 
-async function getApi(url) {
+async function getApi() {
   const response = await fetch(url);
   const data = await response.json();
-  console.log("data");
+  const dataList = data.results;
+  /*   console.log(data); */
+  console.log(dataList);
+  for (let i = 0; i < dataList.length; i++) {
+    console.log(dataList[i].name);
+    console.log(dataList[i].rating);
+    console.log(dataList[i].tags.length);
+    if (i === 7) {
+      break;
+    }
+  }
 }
 getApi();
