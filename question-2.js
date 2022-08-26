041ed1668ca72a03441c6486e14af8795e8adc4e
ddyrnes <dev.dyrnes@gmail.com>
2022-08-26 10:43:08 +0200

    Added css

--- question-2.js
+++ question-2.js
@@ -4,28 +4,29 @@ url = `https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-ratin
 const container = document.querySelector(".container");
 
 async function getApi() {
-  const response = await fetch(url);
-  const data = await response.json();
-  const dataList = data.results;
-  container.innerHTML = "";
-  for (let i = 0; i < dataList.length; i++) {
-    console.log(dataList[i].name);
-    console.log(dataList[i].rating);
-    console.log(dataList[i].tags.length);
-    if (i === 7) {
-      break;
-    }
-    container.innerHTML += `
-    <ul>
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    const dataList = data.results;
+    container.innerHTML = "";
+    for (let i = 0; i < dataList.length; i++) {
+      if (i === 7) {
+        break;
+      }
+      container.innerHTML += `
+    <ul class="ul-container">
     <li> Name of Author: ${dataList[i].name} </li>
     <li> Rating: ${dataList[i].rating} </li>
     <li> Number of tags recieved: ${dataList[i].tags.length} </li>
     </ul>
     `;
+    }
+  } catch (error) {
+    container.innerHTML = "";
+    container.innerHTML = `<p class="ul-container"> An error occoured.</p>`;
   }
 }
 getApi();
-console.log("test");
 //
 
 // getCollection("test tes".then(console.log(test)));
@@ -34,4 +35,4 @@ console.log("test");
 // Several smaller files > one big file
 
 // Handle HTML with template - not learned yet - learn?
-hello = () => console.log("hello");
+// hello = () => console.log("hello");
