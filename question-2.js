ea5db13a1129863d11db066acd5bfd8a875701ab
ddyrnes <dev.dyrnes@gmail.com>
2022-08-26 09:51:56 +0200

    Added html

--- question-2.js
+++ question-2.js
@@ -1,12 +1,13 @@
 apiKey = "1adb635733d147ed887a956df85f5898";
 url = `https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=${apiKey}`;
 
+const container = document.querySelector(".container");
+
 async function getApi() {
   const response = await fetch(url);
   const data = await response.json();
   const dataList = data.results;
-  /*   console.log(data); */
-  console.log(dataList);
+  container.innerHTML = "";
   for (let i = 0; i < dataList.length; i++) {
     console.log(dataList[i].name);
     console.log(dataList[i].rating);
@@ -14,10 +15,17 @@ async function getApi() {
     if (i === 7) {
       break;
     }
+    container.innerHTML += `
+    <ul>
+    <li> Name of Author: ${dataList[i].name} </li>
+    <li> Rating: ${dataList[i].rating} </li>
+    <li> Number of tags recieved: ${dataList[i].tags.length} </li>
+    </ul>
+    `;
   }
 }
 getApi();
-
+console.log("test");
 //
 
 // getCollection("test tes".then(console.log(test)));
